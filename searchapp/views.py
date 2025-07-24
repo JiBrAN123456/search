d8cb217974b54f2937f9c5f41ae3a20f022ff69a
Mohammad Jibran <yunisquadri99@gmail.com>
2025-07-25 03:16:39 +0530

    more

--- searchapp/views.py
+++ searchapp/views.py
@@ -1,7 +1,26 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from django.db import models
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @action(detail=False, methods=['get'], url_path='search')
+    def search(self, request):
+        query = request.query_params.get('q', '')
+
+        if query:
+            products = Product.objects.filter(
+                models.Q(name__icontains=query) |
+                models.Q(description__icontains=query)
+            )
+        else:
+            products = Product.objects.none()
+
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data)
